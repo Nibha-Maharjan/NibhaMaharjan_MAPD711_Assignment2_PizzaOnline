91282fb3c5ce376fd471c67ee46ae43ad203c733
Nibha Maharjan <nibhamaharjan@yahoo.com>
2023-10-13 15:20:00 -0400

    Added some new colors

--- app/src/main/java/com/example/nibhamaharjan_mapd711_assignment2_pizzaonline/checkout.kt
+++ app/src/main/java/com/example/nibhamaharjan_mapd711_assignment2_pizzaonline/checkout.kt
@@ -1,9 +1,14 @@
 package com.example.nibhamaharjan_mapd711_assignment2_pizzaonline
 
+import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 
 class checkout : AppCompatActivity() {
@@ -31,8 +36,34 @@ class checkout : AppCompatActivity() {
         val message = "Selected Toppings: $selectedToppings"
         toppingsAdded.text = message
 
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.ctype,
+            android.R.layout.simple_spinner_item
+            ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+                spinner.adapter = adapter
+            }
+
+
+
 //
 //        text1.text = "You have ordered a $pizzNam Pizza in $pizzSiz and your toppings are:"
 //        toppingsAdded.text = selectedToppings?.joinToString(", ") ?: "No toppings selected"
+    }
+}
+class SpinnerActivity : Activity(), AdapterView.OnItemSelectedListener {
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item is selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos).
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback.
     }
 }
